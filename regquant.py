f2ed6ae311b73a74bc876db2b1d7240a28d5dcf1
regan <rimavie@gmail.com>
2022-02-10 13:00:26 +0100

    mounting v2

--- regquant.py
+++ regquant.py
@@ -1,10 +1,48 @@
 
 #from random import randrange
 import time
+#import numpy as np
 #time.sleep(0.1) #dort 0.1 seconde
 
 
 pi=3.14159265
+e=2.71828182846
+
+def Rpow(a,n): #a quelconque et  n entier
+     val=1
+     for i in range(n):
+        val = val * a
+     return val
+
+def Rfact(n):# factoriel n
+     if n < 0 :
+          print("demande la factoriel d'une valeur négative")
+          return 1 
+     val = 1
+     for i in range(n):
+          val *=(i+1)
+     return val
+
+def Rexp(a):
+     exp_uplet=[e,e*e,e**4,e**8,e**16,e**32,e**64,e**128,e**256]
+     exp_minuplet=[1.6487212707,1.28402541669,1.13314845307,1.06449445892,
+                   1.0317434075,1.01574770859,1.00784309721,1.00391388934, #2^(-8)
+                   1.00195503359,1.00097703949,1.00048840048,1.00024417043,
+                   1.00012207776,1.00006103702,1.00003051804,1.00001525891, #2^(-16)
+                   1.00000762942,1.0000038147,1.00000190735,1.00000095367]
+     exp_minuplet.reverse()
+     expmmax=exp_minuplet + exp_uplet
+     def myexp(x):
+          if x==0: return 1 #on peut avoir exp(pow(2,i)) et cela ne fait jamais 0
+          val=1
+          n=len(exp_minuplet) #Cela permet de gerer des nombres à virgule
+          for i in range(len(expmmax)):
+             alpha=(int(x*pow(2,n)) & Rpow(2,i))
+             if (alpha!=0): val = val * expmmax[i]
+          return val
+     
+     if a < 0: return 1/myexp(-a)
+     return myexp(a)
 
 def Rsqrt(a,eps): #esp is the precision required
 	#returns sqrt(a); We use Newton's algorithm
@@ -81,13 +119,13 @@ class Obj3D:
             #Nature est 'sphere' 'cube' 'cylinder' etc...
             self.nature='unknown object'
             self.handle=[]
-    #Version précédente: mydisp=Display(can1,(200,200,500),(200,-400,-1000)) #Canevas d'affichage et point de fuite
-            self.ptf=(200,200,500)
+            self.ptf=(300,250,700)
             self.can=defcan.can
             self.light=(200,-400,-1000)
             self.faces=[]
             self.pts=[]
             self.ListColor=[]
+            self.zfar=200
             self.color1=1
             self.color2=2
 
@@ -103,16 +141,105 @@ class Obj3D:
             self.p0=Origine #Utile pour la rotation autour de self.p0 = (x0,y0,z0)
     
     def display(self,canevas,PointDeFuite,PointLight): #Si on veut changer les parametres d'affichage.
-            #PointDeFuite est donné par ses coordonnées (x,y,z)
-            #PointLight est le 3-uplet de coordonnées de la source lumineuse
-            self.ptf=PointDeFuite
-            self.can=canevas
-            self.light=PointLight
+         #PointDeFuite est donné par ses coordonnées (x,y,z)
+         #PointLight est le 3-uplet de coordonnées de la source lumineuse
+         self.ptf=PointDeFuite
+         self.can=canevas
+         self.light=PointLight
 
     def color(self,col1,col2):
-            self.color1=col1
-            self.color2=col2
+         self.color1=col1
+         self.color2=col2
 
+    def isosurface(self,Origine,fonct,CubeCoordStart,sizemin):
+         # CubeCoordStart = ((x0,y0,z0),(x1,y1,z1))
+         #             avec x0<x1 y0<y1 et z0<z1
+         # L'idee est de partir d'un cube qui entoure l'objet
+         # ensuite par dicotomie sur l'isosurface à partir:
+         # la frontiere etant lorsque la fonction implicite change de signe
+         # on parvient a obtenir des minicubes qui longent l'isosurface
+         # qui peut etre non connexe.
+         self.nature="isosurface"
+         self.p0=Origine
+         # On commence par couper le cube en 4
+         def divideCube(CubeCoord):
+              #print("CubeCoord=",CubeCoord)
+              ((x0,y0,z0),(x1,y1,z1)) = CubeCoord
+              
+              xm=(x0+x1)/2
+              ym=(y0+y1)/2
+              zm=(z0+z1)/2
+              ListCubes=[]
+              ListCubes.append( ((x0,y0,z0),(xm,ym,zm)) )
+              ListCubes.append( ((x0,ym,z0),(xm,y1,zm)) )
+              ListCubes.append( ((xm,y0,z0),(x1,ym,zm)) )
+              ListCubes.append( ((xm,y0,zm),(x1,ym,z1)) )
+              ListCubes.append( ((x0,ym,zm),(xm,y1,zm)) )
+              ListCubes.append( ((xm,ym,zm),(x1,y1,z1)) )
+              ListCubes.append( ((xm,ym,z0),(x1,y1,zm)) )
+              ListCubes.append( ((x0,y0,zm),(xm,ym,z1)) )
+              return ListCubes
+         
+         def divideAllCubes(listCube):
+              #print("DivideAllCubes=",listCube)
+              NewListCube=[]
+              for i in range (len(listCube)):
+                  cube=listCube[i]
+                  #print("cube de DivideAllCubes=",cube)
+                  NewListCube.extend(divideCube(cube))
+              return NewListCube
+                              
+         def Eliminate_Useless_Cubes(ListCube,function):
+              # La fonction correspond a la fonction implicite de la surface
+              # Si la valeur de la fonction implicite change de signe pour differents
+              # sommets du cube, dans ce cas, on conserve le cube
+              # Voici 3 flags qui prennent la valeur 1 lorsque la fonction
+              # implicite prend les differents signes
+              sign_plus = False
+              sign_minus = False
+              #equal_zero = False
+              ResultCube=[]
+              (xo,yo,zo)=self.p0
+              def RTP(x,y,z):
+                   theta=x
+                   phi=y
+                   return function(x,y)
+              for i in range(len(ListCube)):
+                   #print("ListCube=",ListCube)
+                   ((x0,y0,z0),(x1,y1,z1)) = ListCube[i]
+                   a = RTP(x0-xo,y0-yo,z0-zo)
+                   b = RTP(x0-xo,y0-yo,z1-zo)
+                   c = RTP(x0-xo,y1-yo,z0-zo)
+                   d = RTP(x0-xo,y1-yo,z1-zo)
+                   e = RTP(x1-xo,y0-yo,z0-zo)
+                   f = RTP(x1-xo,y0-yo,z1-zo)
+                   g = RTP(x0-xo,y0-yo,z0-zo)
+                   h = RTP(x0-xo,y0-yo,z1-zo)
+                   if ( (a+b+c+d+e+f+g+h) >= True and (a and b and c and d and e and f and g == False)):
+                        resultCube.append(ListCube[i])
+              return ResultCube
+                              
+         Cube= CubeCoordStart
+         print("cube1=",Cube)
+         size = sizemin+1 #On initialise au dessus de sizemin
+         while size >= sizemin:
+              listCubes = divideAllCubes(Cube)
+              Cube.clear()
+              Cube = Eliminate_Useless_Cubes(listCubes,fonct)
+              listCubes.clear()
+              print("cube2=",Cube)
+              (x0,y0,z0),(x1,y1,z1) = Cube[0]
+              size = Rsqrt((x1-x0)*(x1-x0)+(y1-y0)*(y1-y0)+(z1-z0)*(z1-z0))
+         facelist=[] 
+         for i in range(len(Cube)):
+                   (x0,y0,z0),(x1,y1,z1) = Cube[i]
+                   facelist.append( (x1,y0,z0) )
+                   facelist.append( (x1,y0,z1) )
+                   facelist.append( (x1,y1,z0) )
+                   facelist.append( (x1,y1,z1) )
+         self.faces=facelist
+
+              
     def courbe3D(self,Origine,fonct,Nx,Ny):
             self.nature='object created by a curve'
             self.p0=Origine
@@ -320,61 +447,6 @@ class Obj3D:
                                     else: self.handle.append(canevas.create_line(x0,y0,x1,y1,width=2,fill='#aaaaaa'))
             #if self.nature == 'cube' and mode == 'plain':
             if mode == 'plain':
-                    #print('in order')
-                    ordlist=orderlist(self.faces)
-                    #print('out order')
-                    color=['black','#111111','#222222','#333333','#444444','#555555','#666666','#777777','#888888','#999999','#aaaaaa','#bbbbbb','#cccccc','#dddddd','#eeeeee','white']
-                    for i in range(int(len(ordlist)/4)):
-                            (x0,y0,z0)=ordlist[4*i]
-                            (x1,y1,z1)=ordlist[4*i+1]
-                            (x2,y2,z2)=ordlist[4*i+2]
-                            (x3,y3,z3)=ordlist[4*i+3]
-                            xm=int((x0+x1+x2+x3)/4)
-                            ym=int((y0+y1+y2+y3)/4)
-                            zm=int((z0+z1+z2+z3)/4)
-                            ux,uy,uz=x1-x0,y1-y0,z1-z0
-                            vx,vy,vz=x3-x0,y3-y0,z3-z0
-                            #Redéfinition de x0,y0,...
-                            (X0,Y0)=Conv3Dto2D( ordlist[4*i] )
-                            (x1,y1)=Conv3Dto2D( ordlist[4*i+1] )
-                            (x2,y2)=Conv3Dto2D( ordlist[4*i+2] )
-                            (x3,y3)=Conv3Dto2D( ordlist[4*i+3] )
-                            pts=[(X0,Y0),(x1,y1),(x2,y2),(x3,y3),(X0,Y0)]
-                            #produit vectoriel
-                            wx=uy*vz-uz*vy
-                            wy=uz*vx-ux*vz
-                            wz=ux*vy-uy*vx
-                            NormW=Rsqrt(wx*wx+wy*wy+wz*wz,0.01)
-                            #vecteur normal au rectangle
-                            if NormW==0:
-                                    #print('NormW=0. Set to 1.')
-                                    NormW=1
-                            wx=wx/NormW
-                            wy=wy/NormW
-                            wz=wz/NormW
-                            #scalaire le vecteur lié au point de fuite
-                            (xl,yl,zl)=self.light #On récupère les coordonnées du point de fuite	
-                            Lx,Ly,Lz= (xl-xm , yl-ym, zl-zm)
-                            NormL=Rsqrt(Lx*Lx+Ly*Ly+Lz*Lz,0.01)
-                            #NormL=1
-                            if NormL==0:
-                                    #print('NormL=0. Set to 1.')
-                                    NormL=1
-                            Lx=Lx/NormL
-                            Ly=Ly/NormL
-                            Lz=Lz/NormL
-                            prod_scal=wz
-                            #prod_scal=-(Lz-abs(Lx*wx+Ly*wy+Lz*wz)*wz) #Produit scalaire avec -ez de L+2w
-                            if prod_scal < 0: prod_scal=0 
-                            #print('prod_scal=',prod_scal*20)
-                            col=int(15*prod_scal)
-                            if col >= 15: col=15
-                            Outcol=col-1
-                            if Outcol < 0: Outcol=0
-                            #print(col)
-                            self.handle.append(canevas.create_polygon(pts,width=2,outline=color[Outcol],fill=color[col]))
-                                    #else: self.handle.append(canevas.create_line(x0,y0,x1,y1,width=2,fill='#aaaaaa'))
-            if mode == 'allwhite':
                     ordlist=orderlist(self.faces)
                     #print("ordlist (size = ", len(ordlist)," )= ",ordlist)
                     
@@ -411,20 +483,20 @@ class Obj3D:
                             if NormW==0:
                                     #print('NormW=0. Set to 1.')
                                     NormW=1
-                            wx=wx/NormW
-                            wy=wy/NormW
+                            #wx=wx/NormW
+                            #wy=wy/NormW
                             wz=wz/NormW
                             #scalaire le vecteur lié au point de fuite
-                            (xl,yl,zl)=self.light #On récupère les coordonnées du point de fuite	
-                            Lx,Ly,Lz= (xl-xm , yl-ym, zl-zm)
-                            NormL=Rsqrt(Lx*Lx+Ly*Ly+Lz*Lz,0.01)
+                            #(xl,yl,zl)=self.light #On récupère les coordonnées du point de fuite	
+                            #Lx,Ly,Lz= (xl-xm , yl-ym, zl-zm)
+                            #NormL=Rsqrt(Lx*Lx+Ly*Ly+Lz*Lz,0.01)
                             #NormL=1
-                            if NormL==0:
+                            #if NormL==0:
                                     #print('NormL=0. Set to 1.')
-                                    NormL=1
-                            Lx=Lx/NormL
-                            Ly=Ly/NormL
-                            Lz=Lz/NormL
+                            #        NormL=1
+                            #Lx=Lx/NormL
+                            #Ly=Ly/NormL
+                            #Lz=Lz/NormL
                             prod_scal=wz
                             #prod_scal=-(Lz-abs(Lx*wx+Ly*wy+Lz*wz)*wz) #Produit scalaire avec -ez de L+2w
                             if prod_scal < 0: prod_scal=-prod_scal 
@@ -436,6 +508,83 @@ class Obj3D:
                             #print(col)
                             self.handle.append(canevas.create_polygon(pts,width=2,outline=color[Outcol],fill=color[col]))
                                     #else: self.handle.append(canevas.create_line(x0,y0,x1,y1,width=2,fill='#aaaaaa'))
+            if mode == 'above':
+                    ordlist=orderlist(self.faces)
+                    #print("ordlist (size = ", len(ordlist)," )= ",ordlist)
+                    
+                    color1=['black','#111111','#222222','#333333','#444444','#555555','#666666','#777777','#888888','#999999','#aaaaaa','#bbbbbb','#cccccc','#dddddd','#eeeeee','white']
+                    color2=['black','#111111','#222222','#222233','#333344','#333355','#444466','#444477','#444488','#505099','#5555aa','#6060bb','#6565cc','#7070dd','#7777ee','#8888FF']
+                    color3=['black','#111111','#222222','#223322','#334433','#335533','#446644','#447744','#448844','#559955','#55aa55','#55bb55','#66cc66','#66dd66','#66ee66','#66FF66']
+                    def similitude(pos):
+                            #rotation autour de x pour bien voir par dessus
+                            #attention angle est un 3-uplet
+                            (x,y,z,cl)=pos
+                            (tx,ty,tz)=(-pi/6,0,0)
+                            costx=Rcos(tx)
+                            sintx=Rsin(tx)
+                            (x0,y0,z0)=self.p0 #On conserve le Centre de rotation
+                            dy=y-y0
+                            dz=z-z0
+                            x1 =  x
+                            y1 =  y0 + dy*costx  + dz*sintx
+                            z1 =  z0 - dy*sintx  + dz*costx
+                            return (x1,y1,z1+self.zfar) #zfar est l'eloignement pour voir de loin
+
+                    for i in range(int(len(ordlist)/4)):
+                            (x0,y0,z0,cl1)=ordlist[4*i]
+                            (x1,y1,z1,cl2)=ordlist[4*i+1]
+                            (x2,y2,z2,cl3)=ordlist[4*i+2]
+                            (x3,y3,z3,cl4)=ordlist[4*i+3]
+                             
+                            if (cl1+cl2+cl3+cl4) == 4*self.color1: #Sur un parallelogramme ce n'est pas toujours la meme couleur
+                              color=color1
+                            else:
+                              color=color2
+                            #xm=int((x0+x1+x2+x3)/4)
+                            #ym=int((y0+y1+y2+y3)/4)
+                            #zm=int((z0+z1+z2+z3)/4)
+                            ux,uy,uz=x1-x0,y1-y0,z1-z0
+                            vx,vy,vz=x3-x0,y3-y0,z3-z0
+                            #Redéfinition de x0,y0,...
+                            (X0,Y0)=Conv3Dto2D( similitude(ordlist[4*i] ))
+                            (x1,y1)=Conv3Dto2D( similitude(ordlist[4*i+1] ))
+                            (x2,y2)=Conv3Dto2D( similitude(ordlist[4*i+2] ))
+                            (x3,y3)=Conv3Dto2D( similitude(ordlist[4*i+3] ))
+                            pts=[(X0,Y0),(x1,y1),(x2,y2),(x3,y3),(X0,Y0)]
+                            #produit vectoriel
+                            wx=uy*vz-uz*vy
+                            wy=uz*vx-ux*vz
+                            wz=ux*vy-uy*vx
+                            NormW=Rsqrt(wx*wx+wy*wy+wz*wz,0.01)
+                            #vecteur normal au rectangle
+                            if NormW==0:
+                                    #print('NormW=0. Set to 1.')
+                                    NormW=1
+                            #wx=wx/NormW
+                            #wy=wy/NormW
+                            wz=wz/NormW
+                            #scalaire le vecteur lié au point de fuite
+                            #(xl,yl,zl)=self.light #On récupère les coordonnées du point de fuite	
+                            #Lx,Ly,Lz= (xl-xm , yl-ym, zl-zm)
+                            #NormL=Rsqrt(Lx*Lx+Ly*Ly+Lz*Lz,0.01)
+                            #NormL=1
+                            #if NormL==0:
+                                    #print('NormL=0. Set to 1.')
+                            #        NormL=1
+                            #Lx=Lx/NormL
+                            #Ly=Ly/NormL
+                            #Lz=Lz/NormL
+                            prod_scal=wz
+                            #prod_scal=-(Lz-Rsqrt(abs(Lx*wx+Ly*wy+Lz*wz),0.01)*wz) #Produit scalaire avec -ez de L+2w
+                            if prod_scal < 0: prod_scal=-prod_scal 
+                            #print('prod_scal=',prod_scal*20)
+                            col=int(15*prod_scal)
+                            if col >= 15: col=15
+                            Outcol=col-1
+                            if Outcol < 0: Outcol=0
+                            #print(col)
+                            self.handle.append(canevas.create_polygon(pts,width=2,outline=color[Outcol],fill=color[col]))
+                            #else: self.handle.append(canevas.create_line(x0,y0,x1,y1,width=2,fill='#aaaaaa'))
             canevas.update()
             time.sleep(0.01)
 
